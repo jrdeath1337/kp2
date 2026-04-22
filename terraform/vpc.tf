22abacd71dc742ac62165acf12fc9acc4c8b938f
Dmitriy Petrovich <jrdeath1337@gmail.com>
2026-04-22 20:44:29 +0300

    vpc fix

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -2,10 +2,10 @@ resource "yandex_vpc_network" "network-1" {
   name = "test-network"
 }
 
-resource "yandex_compute_instance" "vm" {
-  name        = "lesson-vm"
-  platform_id = "standard-v1"
-  zone        = var.yc_zone  # Было "ru-central1-a"
-  # ... остальное без изменений
-
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "test-subnet"
+  zone           = var.yc_zone
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
 
