ce3484166a849b24c8c7e0289ab0cb098620c205
Dmitriy Petrovich <jrdeath1337@gmail.com>
2026-04-22 20:38:02 +0300

    fix

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 provider "yandex" {
-  service_account_key_file = "./key.json"
+  service_account_key_file = "key.json"
   cloud_id                 = var.yc_cloud_id
   folder_id                = var.yc_folder_id
   zone                     = var.yc_zone
